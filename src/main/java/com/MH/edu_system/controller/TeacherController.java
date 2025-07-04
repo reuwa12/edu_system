3d0e88792ef9b0df6a0075751ff290c6e36ed76e
USER <mon001024@naver.com>
2025-07-04 15:03:38 +0900

    Teacher Create, Read

---
+++ src/main/java/com/MH/edu_system/controller/TeacherController.java
@@ -0,0 +1,40 @@
+package com.MH.edu_system.controller;
+
+
+import com.MH.edu_system.model.Teacher;
+import com.MH.edu_system.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/teachers")
+@RequiredArgsConstructor
+public class TeacherController {
+    private final TeacherRepository teacherRepository;
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("teachers", teacherRepository.findAll());
+
+        return "teacher-list";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("teacher", new Teacher());
+
+        return "teacher-form";
+    }
+
+    @PostMapping("/add")
+    public  String add(@ModelAttribute Teacher teacher) {
+        teacherRepository.save(teacher);
+
+        return "redirect:/teacher";
+    }
+}
